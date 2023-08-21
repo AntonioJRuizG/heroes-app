a3cf98f034492a6d5c0c2d192e28d44d74b72c3b
Antonio Ruiz <antonioruizgarcia90@gmail.com>
2023-08-21 17:29:59 +0200

    Update heroes layout sidebar menu

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -7,7 +7,7 @@ import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 import { ListPageComponent } from './pages/list-page/list-page.component';
 import { NewItemPageComponent } from './pages/new-item-page/new-item-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
-
+import { MaterialModule } from '../material/material.module';
 
 @NgModule({
   declarations: [
@@ -15,11 +15,8 @@ import { SearchPageComponent } from './pages/search-page/search-page.component';
     HeroPageComponent,
     ListPageComponent,
     NewItemPageComponent,
-    SearchPageComponent
+    SearchPageComponent,
   ],
-  imports: [
-    CommonModule,
-    HeroesRoutingModule
-  ]
+  imports: [CommonModule, HeroesRoutingModule, MaterialModule],
 })
-export class HeroesModule { }
+export class HeroesModule {}
