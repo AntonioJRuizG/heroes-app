eaa9a77d2c77d5b0bc135ef51bfcf36ce0b29598
Antonio Ruiz <antonioruizgarcia90@gmail.com>
2023-08-21 06:03:29 +0200

    Update rounting modules

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -1,10 +1,46 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { NewItemPageComponent } from './pages/new-item-page/new-item-page.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
+import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutPageComponent,
+    children: [
+      {
+        path: 'new-hero',
+        component: NewItemPageComponent,
+      },
+      {
+        path: 'search',
+        component: SearchPageComponent,
+      },
+      {
+        path: 'edit/:id',
+        component: NewItemPageComponent,
+      },
+      {
+        path: 'list',
+        component: ListPageComponent,
+      },
+      {
+        path: ':id', // Esta ruta siempre debe estar al final, porque cualquiera de las anteriores podría ser una id válida y entrar por accidente en HeroPageComponent.
+        component: HeroPageComponent,
+      },
+      {
+        path: '**', // Ruta comodín
+        redirectTo: 'list',
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class HeroesRoutingModule { }
+export class HeroesRoutingModule {}
