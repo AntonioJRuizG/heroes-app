160504062860a298a3f7cd017bf165b39305cb79
Antonio Ruiz <antonioruizgarcia90@gmail.com>
2023-08-23 01:36:14 +0200

    Add form logic & img input

--- src/app/heroes/pages/new-item-page/new-item-page.component.ts
+++ src/app/heroes/pages/new-item-page/new-item-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { Publisher } from '../../interfaces/hero.interface';
+import { Hero, Publisher } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-new-item-page',
@@ -22,4 +23,27 @@ export class NewItemPageComponent {
     { id: 'DC Comics', value: 'DC - Comics' },
     { id: 'Marvel Comics', value: 'Marvel - Comics' },
   ];
+
+  constructor(private heroService: HeroesService) {}
+
+  get currentHero(): Hero {
+    const hero = this.heroForm.value as Hero;
+    return hero;
+  }
+
+  onSubmit(): void {
+    if (this.heroForm.invalid) {
+      return;
+    }
+
+    if (this.currentHero.id) {
+      this.heroService.updateHero(this.currentHero).subscribe((hero) => {
+        // TODO show snackbar
+      });
+    }
+
+    this.heroService.addHero(this.currentHero).subscribe((hero) => {
+      // TODO show snackbar and navigate to heros/edit/id
+    });
+  }
 }
