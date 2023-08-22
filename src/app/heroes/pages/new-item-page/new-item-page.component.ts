e175dadc4f355a6764b83fbfdc0f039f21da89cb
Antonio Ruiz <antonioruizgarcia90@gmail.com>
2023-08-23 00:37:51 +0200

    Add form items type

--- src/app/heroes/pages/new-item-page/new-item-page.component.ts
+++ src/app/heroes/pages/new-item-page/new-item-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Publisher } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-new-item-page',
@@ -8,13 +9,13 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class NewItemPageComponent {
   public heroForm = new FormGroup({
-    id: new FormControl(),
-    superhero: new FormControl(),
-    publisher: new FormControl(),
-    alter_ego: new FormControl(),
-    first_appearance: new FormControl(),
-    characters: new FormControl(),
-    optional_img: new FormControl(),
+    id: new FormControl<string>(''),
+    superhero: new FormControl<string>('', { nonNullable: true }),
+    publisher: new FormControl<Publisher>(Publisher.DCComics),
+    alter_ego: new FormControl(''),
+    first_appearance: new FormControl(''),
+    characters: new FormControl(''),
+    optional_img: new FormControl(''),
   });
 
   publishers = [
