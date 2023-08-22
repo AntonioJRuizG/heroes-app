6d26eea509b81b8ba014e208eb5c834652b7ce0a
Antonio Ruiz <antonioruizgarcia90@gmail.com>
2023-08-23 00:34:14 +0200

    Add controlled form

--- src/app/heroes/pages/new-item-page/new-item-page.component.ts
+++ src/app/heroes/pages/new-item-page/new-item-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-new-item-page',
@@ -6,6 +7,16 @@ import { Component } from '@angular/core';
   styles: [],
 })
 export class NewItemPageComponent {
+  public heroForm = new FormGroup({
+    id: new FormControl(),
+    superhero: new FormControl(),
+    publisher: new FormControl(),
+    alter_ego: new FormControl(),
+    first_appearance: new FormControl(),
+    characters: new FormControl(),
+    optional_img: new FormControl(),
+  });
+
   publishers = [
     { id: 'DC Comics', value: 'DC - Comics' },
     { id: 'Marvel Comics', value: 'Marvel - Comics' },
